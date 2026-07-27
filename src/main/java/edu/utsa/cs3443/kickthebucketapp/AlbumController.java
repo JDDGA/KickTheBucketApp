b926adc5daed51cfb86b760d7c0abc6cb33eac39
Alejandro B <alexbr.9203@gmail.com>
2026-07-26 21:29:53 -0500

    test

--- src/main/java/edu/utsa/cs3443/kickthebucketapp/AlbumController.java
+++ src/main/java/edu/utsa/cs3443/kickthebucketapp/AlbumController.java
@@ -11,7 +11,7 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class AlbumController {
-
+// test 1 commit
     @FXML
     private void handleReturn(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("/edu/utsa/cs3443/kickthebucketapp/layouts/main-menu.fxml"));
